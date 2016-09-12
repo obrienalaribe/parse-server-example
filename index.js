f22a67f893c92546ff88c828ef1371107a9f43e0
Obrien Alaribe <skiveservices@gmail.com>
2016-09-12 22:08:25 +0100

    parse live query

--- index.js
+++ index.js
@@ -11,11 +11,16 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  serverURL: "https://localhost:1337/parse",
+  serverURL: "http://localhost:1337/parse",
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || 'myMasterKey' //Add your master key here. Keep it secret!
+  masterKey: process.env.MASTER_KEY || 'myMasterKey',
+
+  liveQuery: {
+    classNames: ['User', 'Picture', 'Rider', 'Trip']
+  }
+
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -36,3 +41,8 @@ var port = process.env.PORT || 1337;
 app.listen(port, function() {
     console.log('parse-server-example running on port ' + port + '.');
 });
+
+// Initialize a LiveQuery server instance, app is the express app of your Parse Server
+let httpServer = require('http').createServer(app);
+httpServer.listen(port);
+var parseLiveQueryServer = ParseServer.createLiveQueryServer(httpServer);
