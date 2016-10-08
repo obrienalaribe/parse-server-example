e957f29f2ab8b1a722f26c7357c29bc0a8390820
Obrien Alaribe <skiveservices@gmail.com>
2016-10-08 22:26:39 +0100

    prod cert

--- cloud/main.js
+++ cloud/main.js
@@ -24,8 +24,11 @@ Parse.Cloud.define('hello', function(req, res) {
 });
 
 Parse.Cloud.afterSave("Trip", function(request) {
-  // Our "Comment" class has a "text" key with the body of the comment itself
  
+   query = new Parse.Query("Trip");
+
+   console.log(JSON.stringify(request.object))
+
   var pushQuery = new Parse.Query(Parse.Installation);
   pushQuery.equalTo('deviceType', 'ios');
     
