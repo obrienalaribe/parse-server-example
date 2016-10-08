d8d651821047e1cb7d56fce4cf3c675415562b5a
Obrien Alaribe <skiveservices@gmail.com>
2016-10-08 18:18:10 +0100

    changed perm on push cert

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,25 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.afterSave("Trip", function(request) {
+  // Our "Comment" class has a "text" key with the body of the comment itself
+  var commentText = request.object.get('text');
+ 
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo('deviceType', 'ios');
+    
+  Parse.Push.send({
+    where: pushQuery, // Set our Installation query
+    data: {
+      alert: "New trip: " + commentText
+    }
+  }, {
+    success: function() {
+      // Push was successful
+    },
+    error: function(error) {
+      throw "Got an error " + error.code + " : " + error.message;
+    }
+  });
+});
