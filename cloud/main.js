80dfbd9fc5d7ce0127486b5a6117f89ec2a5d79a
Obrien Alaribe <skiveservices@gmail.com>
2016-10-08 22:12:28 +0100

    prod cert

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,26 @@
 
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
+
+
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo('deviceType', 'ios');
+    
+  Parse.Push.send({
+    where: pushQuery, // Set our Installation query
+    data: {
+      alert: "New trip created " 
+    }
+  }, {
+    success: function() {
+      // Push was successful
+        res.success('Push was sent');
+    },
+    error: function(error) {
+      throw "Got an error " + error.code + " : " + error.message;
+    }
+  });
+
 });
 
 Parse.Cloud.afterSave("Trip", function(request) {
