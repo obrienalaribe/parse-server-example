116ce06012df2813d02fb9f3c159684153f588be
Obrien Alaribe <skiveservices@gmail.com>
2016-10-09 21:58:43 +0100

    notify rider

--- cloud/main.js
+++ cloud/main.js
@@ -3,56 +3,36 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 
 
-  var pushQuery = new Parse.Query(Parse.Installation);
-  pushQuery.equalTo('deviceType', 'ios');
-    
-  Parse.Push.send({
-    where: pushQuery, // Set our Installation query
-    data: {
-      alert: "New trip created " 
-    }
-  }, {
-    success: function() {
-      // Push was successful
-        res.success('Push was sent');
-    },
-    error: function(error) {
-      throw "Got an error " + error.code + " : " + error.message;
-    }
-  });
-
 });
 
-Parse.Cloud.afterSave("Trip", function(request) {
- 
-   query = new Parse.Query("Trip");
-   var tripStatus = request.object.get("status")
+Parse.Cloud.define("notifyRiderForAcceptedTrip", function(request, response) {
 
-   if (tripStatus == "Requested") {
-   	   console.log("TRIP REQUESTED OO ");
-   }
+  var params = request.params;
 
-
+  var riderId = params.riderId;
 
   var pushQuery = new Parse.Query(Parse.Installation);
-  pushQuery.equalTo('deviceType', 'ios');
-    
+  pushQuery.equalTo('deviceType', 'ios'); // targeting iOS devices only                                                                                                                                          
+
   Parse.Push.send({
-    where: pushQuery, // Set our Installation query
+  	channels: [riderId],
+    where: pushQuery, // Set our Installation query                                                                                                                                                              
     data: {
-      alert: "New trip created " 
+      alert: "The driver is on their way ",
+      sound: 'default'
+
     }
-  }, {
-    success: function() {
-      // Push was successful
-    },
-    error: function(error) {
-      throw "Got an error " + error.code + " : " + error.message;
-    }
-  });
+  }, { success: function() {
+      console.log("#### PUSH OK");
+  }, error: function(error) {
+      console.log("#### PUSH ERROR " + error.message);
+  }, useMasterKey: true});
+
+  response.success('success');
 });
 
-Parse.Cloud.define("iosPushTest", function(request, response) {
+
+Parse.Cloud.define("broadcastMessage", function(request, response) {
 
   // request has 2 parameters: params passed by the client and the authorized user                                                                                                                               
   var params = request.params;
@@ -68,7 +48,9 @@ Parse.Cloud.define("iosPushTest", function(request, response) {
   Parse.Push.send({
     where: pushQuery, // Set our Installation query                                                                                                                                                              
     data: {
-      alert: "Message: " + messageText
+      alert: "Message: " + messageText,
+      sound: 'default'
+
     }
   }, { success: function() {
       console.log("#### PUSH OK");
