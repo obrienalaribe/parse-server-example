b86521a4580ed635c3d1df9192f839c0846d525b
Obrien Alaribe <skiveservices@gmail.com>
2016-11-10 13:04:53 +0000

    asda

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ if (!databaseUri) {
 var devCertPathForRider = __dirname + '/certs/Dev_Push_Certificate.p12'
 var prodCertPathForRider = __dirname + '/certs/Prod_Push_Certificate.p12'
 
-var devCertPathForDriver = __dirname + '/certs/Driver_Prod_Cert.p12'
+var devCertPathForDriver = __dirname + '/certs/Driver_Push_Cert.p12'
 
 if (!devCertPathForDriver) {
     console.log('MUPPET ITS BECAUSE IT CANNOT READ FILE');
